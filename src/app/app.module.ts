c507993c5a1055083b59a130387afd740455725b
shivam <shivamagrahari0786@gmail.com>
2022-11-30 17:42:28 +0530

    some new add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavTopComponent } from './shair/nav-top/nav-top.component';
+import { NavBottomComponent } from './shair/nav-bottom/nav-bottom.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavTopComponent,
+    NavBottomComponent
   ],
   imports: [
     BrowserModule,
